e82babb64da999c30168fd01948bf300220df5dd
zwemarnhtay <zwemarnhtay@gmail.com>
2025-02-21 17:38:03 +0630

    [task3] pie chart for total amount per status added

---
+++ fta_frontend/js/charts/piechart.js
@@ -0,0 +1,39 @@
+import { fetchSummaryBy } from "../api.js";
+
+async function generatePieChart(){
+    let summaryPerStatus = await fetchSummaryBy('summary-per-status');
+    let status = summaryPerStatus.map(s => s.status);
+    let totalAmounts = summaryPerStatus.map(s => s.total_amount);
+
+    const data = {
+        labels: status,
+        datasets: [
+            {
+                label: 'Total Amount ($)',
+                data: totalAmounts,
+                backgroundColor: ["#fc0539", "#05fcaa", "#e9fa2f"],
+            }
+        ]
+    };
+
+    const ctx = document.getElementById("statusPieChart").getContext("2d");
+    
+    new Chart(ctx, {
+        type: 'pie',
+        data: data,
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                position: 'top',
+                },
+                title: {
+                display: true,
+                text: 'Transaction Amounts by Status'
+                }
+            }
+        },
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => generatePieChart());
